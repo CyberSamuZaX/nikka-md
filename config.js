fc64ebe1b3f0dc75f8eee4f5c8d2c2338c8fb729
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-04-03 22:10:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
   AUTO_READ_STATUS: toBool(process.env.AUTO_READ_STATUS) || true,
   AUTO_LIKE_STATUS: toBool(process.env.AUTO_LIKE_STATUS) || true,
   AUTO_LIKE_EMOJI: toBool(process.env.AUTO_LIKE_EMOJI) || true,
-  SESSION_ID:process.env.SESSION_ID || "RXNDBYDb#C68xOpr3ix3o92HoxOAIV9GnxakQlNXDtQ63u5i9AMM",
+  SESSION_ID:process.env.SESSION_ID || "6WxRWQ6B#xXNz-UDvB7aDeOfY1_0ZdR68dxZ5BBRaBuh2Foh3l7A",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[?]',
   PRESCENCE: process.env.PRESCENCE || "typing",
